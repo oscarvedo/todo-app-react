77b4240fe587c7865a0572e8f7c0a4b4b78d0134
oscarvedo <balnicor@gmail.com>
2022-05-13 10:24:28 -0500

    Estructura inicial todo app con algunos estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-app'>
+      <div className='freecodecamp-logo-container'>
+        <img
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo' 
+          alt='freeCodeCamp logo' />
+      </div>
+      <div className='todo-main-list'>
+        <h1>Mis Tareas</h1>
+      </div>
     </div>
   );
 }
